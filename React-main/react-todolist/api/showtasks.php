db435e8b0ff157ba2bf00e1ef49933e109886738
Oussama_Ennasser <76487485+oussama20coder@users.noreply.github.com>
2021-06-30 13:40:14 +0100

    Revert "added file"

--- React-main/react-todolist/api/showtasks.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbh = new PDO("mysql:host=localhost;dbname=todo_list_app","root","");
-$sql = " SELECT * FROM tasks ";
-$TaskQuery = $dbh->query($sql);
-$getTask = $TaskQuery->fetchAll(PDO::FETCH_ASSOC);
-print_r(json_encode($getTask));
-?>
